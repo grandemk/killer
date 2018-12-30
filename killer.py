93322617c32d27f12473d45529f96968c402bcf7
Kevin Grandemange <grandemange.kevin@gmail.com>
2018-12-30 22:21:11 +0100

    command line args

--- killer.py
+++ killer.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
 import random
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("seed", help="seed for the order of the killer")
+    return parser.parse_args()
 
 def main():
+    args = parse_args()
     players = ["antoine", "elisa", "lucas", "gabriel", "sabine", "benoit", "michou", "julie", "morgane", "pier", "codex", "alex", "tom", "theo", "dorian", "quentin", "etienne", "kevin"]
-    random.seed(0)
+    random.seed(args.seed)
     random.shuffle(players)
     print(players)
 
